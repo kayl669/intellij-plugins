271dd2fa2503ba7878287919ab55243ce63e9b68
Mickaël SEBIRE <misebire@axway.com>
2017-08-17 08:16:25 +0200

    Add option Charm.Flex.SkipFileRefreshCharm.Flex.SkipFileRefresh in FlexBuildConfigurationManagerImpl.java

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/model/impl/FlexBuildConfigurationManagerImpl.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/model/impl/FlexBuildConfigurationManagerImpl.java
@@ -1,6 +1,7 @@
 package com.intellij.lang.javascript.flex.projectStructure.model.impl;
 
 import com.intellij.ProjectTopics;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.lang.javascript.flex.projectStructure.model.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
@@ -170,7 +171,9 @@ public class FlexBuildConfigurationManagerImpl extends FlexBuildConfigurationMan
   }
 
   static void resetHighlighting(Project project) {
-    ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), false, true);
+    if (!PropertiesComponent.getInstance().getBoolean("Charm.Flex.SkipFileRefresh", true)) {
+      ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), false, true);
+    }
   }
 
   private FlexBuildConfigurationImpl[] getValidatedConfigurations(Collection<? extends FlexBuildConfigurationImpl> configurations) {
@@ -208,7 +211,7 @@ public class FlexBuildConfigurationManagerImpl extends FlexBuildConfigurationMan
         configList.get(i).setName(uniqueNames.get(i));
       }
     }
-    
+
     return configList.toArray(new FlexBuildConfigurationImpl[0]);
   }
 
